cbe5aabd9d811ab684f076cca724b47d0990a38e
James Sandford Smith <jesandfordsmith@gmail.com>
2023-03-22 09:28:40 +0000

    fixed gamespeed increasing on gameover

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ canvas.width = 400;
 canvas.height = 650;
 let gameState = true;
 let gameAr = [];
+let game = 0;
 let gameOverTimer = 3000;
 class Game {
     constructor(width, height){
@@ -47,19 +48,23 @@ class Game {
         // document.location.reload();
     }
 }
-let game = new Game(canvas.width, canvas.height)
+// let game = new Game(canvas.width, canvas.height)
 // const game = new Game(canvas.width, canvas.height);
 // gameStateHandler() ;
 function animate(){
     if(gameState) {
     ctx.clearRect(0,0, canvas.width, canvas.height);
-        /* game.update();
-        game.draw(ctx); */
-        gameAr[0].update();
-        gameAr[0].draw(ctx);
+        game.update();
+        game.draw(ctx);
+        /* gameAr[0].update();
+        gameAr[0].draw(ctx); */
         requestAnimationFrame(animate);
     }
     else {
+        if(gameOverTimer <= 0){
+            gameOverTimer = 3000;
+            gameStateHandler();
+        }
         ctx.clearRect(0,0, canvas.width, canvas.height);
         ctx.fillStyle = "black";
         ctx.fillRect(0,0, canvas.width, canvas.height)
@@ -68,12 +73,7 @@ function animate(){
         ctx.fillText("GAME OVER",20,canvas.height/2)
         ctx.font = "40px Comic Sans MS";
         ctx.fillText(String("NEW GAME IN "+Math.ceil(gameOverTimer/1000)),35,canvas.height/2 + 100);
-        gameOverTimer -= 4;
-        
-        if(gameOverTimer <= 0){
-            gameOverTimer = 3000;
-            gameStateHandler();
-        }
+        gameOverTimer -= 4;      
         requestAnimationFrame(animate);
     }
 }
@@ -84,15 +84,19 @@ function gameStateHandler() {
     
     if(gameState)
     {
-        gameAr.push(game);
+        game = new Game(canvas.width, canvas.height)
+        console.log("game start");
+        // gameAr.push(game);
         animate();
     }
     else{
-        // console.log("game over handler");
-        gameAr.splice(0,1);
-        gameState = true;
+        console.log("game over handler",gameAr.length);
+        // gameAr.splice(0,1);
+        game = 0;
         game = new Game(canvas.width, canvas.height)
-        gameAr.push(game);
+        gameState = true;
+        // game = new Game(canvas.width, canvas.height)
+        // gameAr.push(game);
         animate();
     }
 }
